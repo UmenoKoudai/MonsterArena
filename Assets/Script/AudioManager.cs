33f01db4037aca3b07489616bbeb783c7ffb4aa9
UmenoKoudai <umenokoudai0805@gmail.com>
2024-06-02 20:37:46 +0900

    [update]ゲームループ作成 [add]AudioManager作成中

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -1,19 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField]
-    private AudioClip[] _clip;
 
-    public enum Clip
+    private BGM _bgmClass;
+    public BGM BGMClass => _bgmClass;
+
+    private static AudioManager _instance;
+    public static AudioManager Instance
     {
-        System1,
-        System2,
-        System3,
-        Select,
-        Attack,
-        Move,
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+            }
+            return _instance;
+        }
+    }
+
+    [Serializable]
+    public class BGM
+    {
+        [SerializeField, Tooltip("BGM‚ð–Â‚ç‚·AudioSource")]
+        private AudioSource _bgmSound;
+        [SerializeField]
+        private AudioClip[] _bgmClip;
+
+        public enum BGMClip
+        {
+            Title,
+            Game,
+            Result,
+        }
+
+        public void Play(BGMClip clip)
+        {
+            _bgmSound.PlayOneShot(_bgmClip[(int)clip]);
+        }
+
+    }
+
+    [Serializable]
+    public class SE
+    {
+        [SerializeField, Tooltip("SE‚ð–Â‚ç‚·AudioSource")]
+        private AudioSource _seSound;
     }
 }
